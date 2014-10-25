cb4edf35659dc0eab3a3d092479a17831f7cd992
Andrei Neculau <andrei.neculau@gmail.com>
2014-10-25 16:37:18 +0200

    add hex info (not working)

---
+++ package.exs
@@ -0,0 +1,39 @@
+defmodule KATT.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :katt,
+     version: "1.3.0",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:lhttpc, github: "waisbrot/lhttpc"},
+     {:mochijson3, github: "tophitpoker/mochijson3"},
+     {:meck, "~> 0.8.2", only: :test, env: :test},
+     {:neotoma, github: "seancribbs/neotoma", only: :dev, env: :dev}]
+  end
+
+  defp description do
+    """
+    KATT (Klarna API Testing Tool) is an HTTP-based API testing tool for Erlang.
+    """
+  end
+
+  defp package do
+    [files: ["AUTHORS",
+             "GNUMakefile",
+             "LICENSE",
+             "README.md",
+             "doc",
+             "include",
+             "package.exs",
+             "priv",
+             "rebar.config",
+             "src"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/for-GET/katt"}]
+   end
+end
