a8b9a4703846046dc4f7c76f4477f7d04e814dfe
Andrei Neculau <andrei.neculau@gmail.com>
2015-02-07 18:16:55 +0100

    move escript into standalone module. prep for escriptize

--- src/katt_cli.erl
+++ src/katt_cli.erl
@@ -1,12 +1,43 @@
-#!/usr/bin/env escript
-%%! -noinput
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Copyright 2014- AUTHORS
+%%%
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%
+%%% @copyright 2014- AUTHORS
+%%%
+%%% @doc Klarna API Testing Tool
+%%%
+%%% CLI.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%_* Module declaration =======================================================
+-module(katt_cli).
+
+%%%_* Exports ==================================================================
+%% API
+-export([main/1]).
+
+%%%_* API ======================================================================
 
 main([]) ->
     main(["--help"]);
 main(["-h"]) ->
     main(["--help"]);
 main(["--help"]) ->
-    io:fwrite("Usage: ~s [--json] file.katt param=string param:=non_string ~n", [escript:script_name()]);
+    io:fwrite( "Usage: ~s [--json] file.katt param=string param:=non_string ~n"
+             , [escript:script_name()]
+             );
 main(["--json", ScenarioFilename|Params]) ->
     KattResult = katt_run(ScenarioFilename, Params),
     JsonResult = katt_util:run_result_to_mochijson3(KattResult),
@@ -32,9 +63,7 @@ main([ScenarioFilename|Params]) ->
             halt(1)
     end.
 
-%%%
-%%% HELPERS
-%%%
+%%%_* Internal =================================================================
 
 katt_run(ScenarioFilename, Params0) ->
     Params = parse_params(Params0),
